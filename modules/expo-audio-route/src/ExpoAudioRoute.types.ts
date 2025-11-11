0c0118ed38385609fe41e4072ff84ecb9f54c392
Hassan Khan <hassan@expo.dev>
2025-11-11 14:55:23 +0000

    Implement the JavaScript API

---
+++ modules/expo-audio-route/src/ExpoAudioRoute.types.ts
@@ -0,0 +1,1 @@
+export type AudioRoute = "speaker" | "wiredHeadset" | "bluetooth" | "unknown";
--- modules/expo-audio-route/src/ExpoAudioRouteModule.ts
+++ modules/expo-audio-route/src/ExpoAudioRouteModule.ts
@@ -1,6 +1,9 @@
 import { NativeModule, requireNativeModule } from "expo";
+import { AudioRoute } from "./ExpoAudioRoute.types";
 
-declare class ExpoAudioRouteModule extends NativeModule {}
+declare class ExpoAudioRouteModule extends NativeModule {
+  getCurrentRouteAsync(): Promise<AudioRoute>;
+}
 
 // This call loads the native module object from the JSI.
 export default requireNativeModule<ExpoAudioRouteModule>("ExpoAudioRoute");
