40a36d57fc94f70bd450275241d9c5bb6f6f1453
Hassan Khan <hassan@expo.dev>
2025-11-11 15:43:28 +0000

    Simplify app code with `useEventListener`

--- App.tsx
+++ App.tsx
@@ -2,18 +2,14 @@ import { StatusBar } from "expo-status-bar";
 import React from "react";
 import { Button, StyleSheet, Text, View } from "react-native";
 import ExpoAudioRoute, { AudioRoute } from "./modules/expo-audio-route";
+import { useEventListener } from "expo";
 
 export default function App() {
   const [audioRoute, setAudioRoute] = React.useState<AudioRoute>("unknown");
-  React.useEffect(() => {
-    const sub = ExpoAudioRoute.addListener("onAudioRouteChange", ({ route }) => {
-      setAudioRoute(route);
-    });
 
-    return () => {
-      sub.remove();
-    };
-  }, []);
+  useEventListener(ExpoAudioRoute, "onAudioRouteChange", ({ route }) => {
+    setAudioRoute(route);
+  });
 
   return (
     <View style={styles.container}>
