527dfe13427c0b7fdd33f5d99c789f81a1f9d464
Hassan Khan <hassan@expo.dev>
2025-11-11 14:35:20 +0000

    Listen to an event from the Expo Module

--- App.tsx
+++ App.tsx
@@ -1,14 +1,31 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { Alert, Button, StyleSheet, Text, View } from 'react-native';
 import ExpoAudioRoute from "./modules/expo-audio-route";
 
 export default function App() {
+  React.useEffect(() => {
+    const sub = ExpoAudioRoute.addListener("onChange", (ev) => {
+      Alert.alert("Event received", ev.value);
+    });
+
+    return () => {
+      sub.remove();
+    };
+  }, []);
+
   return (
     <View style={styles.container}>
       <Text>Open up App.tsx to start working on your app!</Text>
       <StatusBar style="auto" />
       <Text>{ExpoAudioRoute.PI}</Text>
       <Text>{ExpoAudioRoute.hello()}</Text>
+      <Button
+        title="Click me"
+        onPress={() => {
+          ExpoAudioRoute.setValueAsync("Hello World");
+        }}
+      />
     </View>
   );
 }
