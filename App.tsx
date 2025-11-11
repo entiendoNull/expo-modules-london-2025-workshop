28763b15ed8fa1bbe0b1ed2151824aec221fb16e
Hassan Khan <hassan@expo.dev>
2025-11-11 14:33:53 +0000

    Use the imperative API from the Expo Module

--- App.tsx
+++ App.tsx
@@ -1,11 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import ExpoAudioRoute from "./modules/expo-audio-route";
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.tsx to start working on your app!</Text>
       <StatusBar style="auto" />
+      <Text>{ExpoAudioRoute.PI}</Text>
+      <Text>{ExpoAudioRoute.hello()}</Text>
     </View>
   );
 }
